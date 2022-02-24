5a042989def80b302b1226eaabfdc8f4830f609f
patrickacheung <patrickcheungdev@gmail.com>
2022-02-23 19:07:28 -0800

    add new function

--- src/main/java/patche/amp_json/JsonProcessingApp.java
+++ src/main/java/patche/amp_json/JsonProcessingApp.java
@@ -22,10 +22,12 @@ public class JsonProcessingApp {
 
     private final AmpJsonReader jsonReader;
     private final Map<String, Set<String>> extensionToFilenames;
+    private final Map<Long, Map<String, Integer>> hourlyShas;
 
     public JsonProcessingApp(AmpJsonReader ampJsonReader) {
         jsonReader = ampJsonReader;
         extensionToFilenames = new HashMap<>();
+        hourlyShas = new HashMap<>();
     }
 
     /**
@@ -62,6 +64,37 @@ public class JsonProcessingApp {
         }
     }
 
+    public void getUniqueSha(String filePath) {
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (jsonReader.isValidFileMetaData(line)) {
+                    FileMetaData metaData = jsonReader.fromJson(line);
+                    Date date = new Date(metaData.getTimestamp() * 1000); // epoch seconds
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(date);
+
+                    String sha = metaData.getSha();
+                    long hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+                    //LOG.info(hourOfDay);
+                    Map<String, Integer> hourToSha = hourlyShas.getOrDefault(hourOfDay, new HashMap<>());
+                    hourToSha.put(sha, hourToSha.getOrDefault(sha, 0) + 1);
+
+                    hourlyShas.put(hourOfDay, hourToSha);
+                }
+            }
+
+            for (Map.Entry<Long, Map<String, Integer>> entry : hourlyShas.entrySet()) {
+                LOG.info(entry.getKey() + " o'clock:");
+                for (Map.Entry<String, Integer> e1 : entry.getValue().entrySet()) {
+                    LOG.info("sha: " + e1.getKey() + ", count: " + e1.getValue());
+                }
+            }
+        } catch (IOException e) {
+            LOG.error("failed to read file: " + filePath);
+        }
+    }
+
     private void printExtensionsAndUniqueCount(Map<String, Set<String>> extAndFilenameCounts) {
         for (Map.Entry<String, Set<String>> entry : extAndFilenameCounts.entrySet()) {
             LOG.info("{}: {}", entry.getKey(), entry.getValue().size());
@@ -73,6 +106,8 @@ public class JsonProcessingApp {
         AmpJsonReader jsonReader = new AmpJsonReader();
         JsonProcessingApp app = new JsonProcessingApp(jsonReader);
 
-        app.processLogFile(resourceDir + "/test.log");
+        app.processLogFile(resourceDir + "/test.example");
+
+        app.getUniqueSha(resourceDir + "/inputData_std.jsonl");
     }
 }
